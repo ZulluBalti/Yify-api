d7e1b94fd740e38f43a5b4b655b366c4f903a9fa
Zullu <giantmirchu@gmail.com>
2018-06-07 21:20:45 +0500

    Fixe some style issue

--- Assests/script.js
+++ Assests/script.js
@@ -5,7 +5,6 @@ form.addEventListener('submit', function(event){
     search();
 });
 
-
 function search(){
     let val = document.querySelector('#searchBox').value;
     if(val != ''){
@@ -30,35 +29,17 @@ function display(mydata){
         cont.appendChild(para);
     }else{
     for(var x=0;x<mydata.data.movie_count;x++){    
-        // appending image
-        let img = document.createElement('img');
-        let imgDiv = document.createElement('div');
-        let imgSrc = mydata.data.movies[x].medium_cover_image;
-        img.setAttribute('src', imgSrc);img.setAttribute('width', '130');img.setAttribute('height', '120');
-        imgDiv.appendChild(img);
+        //appending rows to #movies
         
-        // appending title
-        let h3 = document.createElement('h3');
-        let title = mydata.data.movies[x].title + " (" + mydata.data.movies[x].year + ")";
-        let text_node = document.createTextNode(title);
-        h3.appendChild(text_node);
+        let rows = document.createElement("div");
+        rows.classList.add("row");
+        rows.innerHTML = '<div class="col-2"><img class="poster" width="150" height="200" src="' + mydata.data.movies[x].medium_cover_image + 
+            '"></div><div class="col-10" id="info"><h3>' + mydata.data.movies[x].title + ' (' +
+            mydata.data.movies[x].year + ')</h3><p>' + mydata.data.movies[x].summary + '</p></div>';
         
-        let des = document.createElement('p');
-        let desData= mydata.data.movies[x].summary;
-        let desTextNode = document.createTextNode(desData);
-        des.appendChild(desTextNode);
-        //appended des
-        let div = document.createElement('div');
-        let sec = document.createElement('section');
-        
-        div.setAttribute('class', 'row');
-        imgDiv.setAttribute('class', 'col-2');
-        sec.setAttribute('class', 'col-10');
-        
-        sec.appendChild(h3); sec.appendChild(des);
-        div.appendChild(imgDiv);
-        div.appendChild(sec);
-        cont.appendChild(div);        
+        cont.appendChild(rows);
+            
+       
     }}
     
     
