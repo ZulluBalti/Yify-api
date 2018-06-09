3e06a0e0ef691353777677a9ce773b96ca4a153c
Zullu <giantmirchu@gmail.com>
2018-06-09 23:31:00 +0500

    finished styling box + 3d issue

--- Assests/script.js
+++ Assests/script.js
@@ -58,18 +58,19 @@ function display(mydata){
 }
  
 function downDiv(data){
-    let mag7 = "magnet:?xt=urn:btih:"+ data.torrents[0].hash + "&dn=" + encUrl + "&tr=udp://glotorrents.pw:6969/announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://torrent.gresille.org:80/announce&tr=udp://tracker.openbittorrent.com:80";
-    let mag8 = "magnet:?xt=urn:btih:"+ data.torrents[1].hash + "&dn=" + encUrl + "&tr=udp://glotorrents.pw:6969/announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://torrent.gresille.org:80/announce&tr=udp://tracker.openbittorrent.com:80";
+    let y = data.torrents.length;
+    let mag7 = "magnet:?xt=urn:btih:"+ data.torrents[y-2].hash + "&dn=" + encUrl + "&tr=udp://glotorrents.pw:6969/announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://torrent.gresille.org:80/announce&tr=udp://tracker.openbittorrent.com:80";
+    let mag8 = "magnet:?xt=urn:btih:"+ data.torrents[y-1].hash + "&dn=" + encUrl + "&tr=udp://glotorrents.pw:6969/announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://torrent.gresille.org:80/announce&tr=udp://tracker.openbittorrent.com:80";
     box = document.createElement("div");
     box.classList.add("down");
     box.classList.add("row");
     box.classList.add("text-center");
     box.setAttribute("id", "box");
-    box.innerHTML = '<h3 class="col-12">' + data.title + '</h3><div class="col-6"><h4>720p</h4><a href=' + mag7 + '><span class="fa fa-magnet"></span></a><a href="' +
-                    data.torrents[0].url + 
-                    '"><span class="fa fa-download"></span></a></div><div class="col-6"><h4>1080p</h4><a href=' +mag8+ '"><span class="fa fa-magnet"></span></a><a href="' + 
-                    data.torrents[1].url + 
-                    '"><span class="fa fa-download"></span></a></div><span onclick="hide()"    id="close" class="fa fa-close"></span>';
+    box.innerHTML = '<span class="col-12" id="h3">' + data.title + '</span><div class="col-6"><h4>720p</h4><a href=' + mag7 + '><span class="fa fa-magnet"> </span></a><a href="' +
+                    data.torrents[y-2].url + 
+                    '"><span class="fa fa-download"></span></a></div><div class="col-6"><h4>1080p</h4><a href=' +mag8+ '"><span class="fa fa-magnet"> </span></a><a href="' + 
+                    data.torrents[y-1].url + 
+                    '"><span class="fa fa-download"></span></a></div><span onclick="hide()"    id="close" class="fa fa-close"></span>';``
     body.appendChild(box);
 }
 
