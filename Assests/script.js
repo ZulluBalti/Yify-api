8e7518af0c326d8706fc364f101c1ba51f4a8373
Zullu <giantmirchu@gmail.com>
2018-06-16 11:04:43 +0500

    added size + genre

--- Assests/script.js
+++ Assests/script.js
@@ -1,14 +1,13 @@
 let form = document.querySelector('form');
 let body = document.querySelector("body");
 let cont = document.querySelector('#movies');
-
 form.addEventListener('submit', function(event){
     event.preventDefault();
     cont.innerHTML = '<img id="loading" src="images/loading.gif" width="50" height="50">';
     search();
 });
 let box;
-let encUrl;
+let encUrl; 
 function search(){
     let val = document.querySelector('#searchBox').value;
     if(val != ''){
@@ -34,12 +33,17 @@ function display(mydata){
     }else{
     for(var x=0;x<mydata.data.movie_count;x++){    
         //appending rows to #movies
+      let genre = '';
+      for(let y=0;y < mydata.data.movies[x].genres.length;y++){
+            genre += mydata.data.movies[x].genres[y] + ', ';
+} 
         
         let rows = document.createElement("div");
         rows.classList.add("row"); rows.setAttribute("id", "row-"+x);
         rows.innerHTML = '<div class="col-2"><img class="poster" width="150" height="200" src="' + mydata.data.movies[x].medium_cover_image + 
             '"></div><div class="col-10" id="info"><h3>' + mydata.data.movies[x].title + ' (' +
-            mydata.data.movies[x].year + ')</h3><p>' + mydata.data.movies[x].summary + '</p></div>';
+            mydata.data.movies[x].year + ')</h3><span class="clearFix text-success"><strong>Genre: </strong>' +
+            genre + '<p>' + mydata.data.movies[x].summary + '</p></div>';
         
         cont.appendChild(rows);
             
@@ -66,12 +70,18 @@ function downDiv(data){
     box.classList.add("row");
     box.classList.add("text-center");
     box.setAttribute("id", "box");
-    box.innerHTML = '<span class="col-12" id="h3">' + data.title + '</span><div class="col-6"><h4>720p</h4><a href=' + mag7 + '><span class="fa fa-magnet"> </span></a><a href="' +
+    box.innerHTML = '<span class="col-12" id="h3">' + data.title +'</span>' + 
+                    '<div class="col-6 ver"><h4>720p</h4><a href=' + mag7 + 
+                    '><span onclick="hide()" class="fa fa-magnet"> </span></a><a href="' +
                     data.torrents[y-2].url + 
-                    '"><span class="fa fa-download"></span></a></div><div class="col-6"><h4>1080p</h4><a href=' +mag8+ '"><span class="fa fa-magnet"> </span></a><a href="' + 
+                    '"><span  onclick="hide()" class="fa fa-download"></span></a><span class="size">Size: ' + data.torrents[y-2].size + '</span></div>' +
+                    '<div class="col-6"><h4>1080p</h4><a href=' + mag8 + 
+                    '"><span  onclick="hide()" class="fa fa-magnet"> </span></a><a href="' + 
                     data.torrents[y-1].url + 
-                    '"><span class="fa fa-download"></span></a></div><span onclick="hide()"    id="close" class="fa fa-close"></span>';``
+                    '"><span  onclick="hide()" class="fa fa-download"></span></a><span class="size">Size: ' + 
+                    data.torrents[y-1].size + '</div><span onclick="hide()"    id="close" class="fa fa-close"></span>';
     body.appendChild(box);
+
 }
 
 function hide(){
