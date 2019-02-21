0c4e2c555d9fffe7ee01c44ef48067b3d6fc2cf6
Seinfeld70 <karamerSeinfeld@gmail.com>
2019-02-21 14:31:53 +0500

    imporved download container and fixed some issues

--- Assests/script.js
+++ Assests/script.js
@@ -1,98 +1,139 @@
-let form = document.querySelector('form');
+let form = document.querySelector("form");
 let body = document.querySelector("body");
-let cont = document.querySelector('#movies');
+let cont = document.querySelector("#movies");
 
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
-    cont.innerHTML = '<img id="loading" src="images/loading.gif" width="50" height="50">';
-    search();
+// Search for movies after submit
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  let val = document.querySelector("#searchBox").value;
+  if (val) {
+    let url = "https://yts.am/api/v2/list_movies.json?query_term=" + val;
+    disGif();
+    search(url);
+  }
 });
+// Show movies on load
+// Making date Year in UI dynamic
 window.addEventListener("load", () => {
-    let d = new Date();
-    document.querySelector(".date__year").textContent = d.getFullYear();
+  let d = new Date();
+  document.querySelector(".date__year").textContent = d.getFullYear();
+  disGif();
+  let url = "https://yts.am/api/v2/list_movies.json";
+  search(url);
+});
+// hide download container/div
+body.addEventListener("click", e => {
+  // If click on down container
+  if (e.target.classList.contains("down__con")) {
+    hideDownCon();
+  }
 });
 
 let box;
 let encUrl;
 
-function search() {
-    let val = document.querySelector('#searchBox').value;
-    if (val != '') {
-        let url = "https://yts.am/api/v2/list_movies.json?query_term=" + val;
-        encUrl = encodeURI(url);
-        fetch(url)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                display(data);
+// Show gif
+function disGif() {
+  cont.innerHTML =
+    '<img id="loading" src="images/loading.gif" width="50" height="50">';
+}
 
-            });
-    }
+async function search(url) {
+  let res = await fetch(url);
+  let data = await res.json();
+  console.log(data.data);
+  display(data.data);
 }
 
 function display(mydata) {
-    cont.innerHTML = '';
-    if (mydata.data.movie_count == 0) {
-        let para = document.createElement('p');
-        para.innerHTML = "<b>Sorry, We didn't found any thing<br>Check the spelling or try someghing else.</b>";
-        cont.appendChild(para);
-    } else {
-        for (var x = 0; x < mydata.data.movie_count; x++) {
-            //appending rows to #movies
-            let genre = '';
-            for (let y = 0; y < mydata.data.movies[x].genres.length; y++) {
-                genre += mydata.data.movies[x].genres[y] + ', ';
-            }
+  cont.innerHTML = "";
+  // If there is no movie
+  if (mydata.movie_count == 0) {
+    let para = document.createElement("p");
+    para.innerHTML =
+      "<b>Sorry, We didn't found any thing<br>Check the spelling or try someghing else.</b>";
+    cont.appendChild(para);
+  } else {
+    mydata.movies.forEach((movie, x) => {
+      //appending rows to #movies
+      let genre = "";
 
-            let rows = document.createElement("div");
-            rows.classList.add("row");
-            rows.setAttribute("id", "row-" + x);
-            rows.innerHTML = '<div class="col-2"><img class="poster" width="150" height="200" src="' + mydata.data.movies[x].medium_cover_image +
-                '"></div><div class="col-10" id="info"><h3>' + mydata.data.movies[x].title + ' (' +
-                mydata.data.movies[x].year + ')</h3><span class="clearFix text-success"><strong>Genre: </strong>' +
-                genre + '<p>' + mydata.data.movies[x].summary + '</p></div>';
+      if (movie.genres) {
+        movie.genres.forEach(g => {
+          genre += `${g}, `;
+        });
+      }
 
-            cont.appendChild(rows);
-
-
-        }
-    }
-    let rowDiv = [];
-    for (i = 0; i < mydata.data.movie_count; i++) {
-        rowDiv[i] = document.querySelector("#row-" + i);
-        rowDiv[i].addEventListener("click", function () {
-            let x = this.getAttribute("id");
-            let i = x.substr(4, 1);
-
-            downDiv(mydata.data.movies[i]);
-        })
-    };
+      let rows = `<div class="row movie__box" id="row-${x}">
+                    <div class="col-2">
+                        <img class="poster" width="150" height="200" src="${
+                          movie.medium_cover_image
+                        }">
+                    </div>
+                    <div class="col-10" id="info">
+                        <h3>${movie.title} (${movie.year})</h3>
+                        <span class="clearFix text-success"><strong>Genre: </strong>${genre}
+                        <p>${movie.summary}</p>
+                    </div>;
+                </div>
+                `;
+      cont.insertAdjacentHTML("afterbegin", rows);
+    });
+  }
+  let movie__boxes = document.querySelectorAll(".movie__box");
+  movie__boxes = Array.from(movie__boxes);
+  movie__boxes.forEach((movie, i) => {
+    movie.addEventListener("click", () => {
+      downDiv(mydata.movies[i]);
+    });
+  });
 }
 
 function downDiv(data) {
-    let y = data.torrents.length;
-    let mag7 = "magnet:?xt=urn:btih:" + data.torrents[y - 2].hash + "&dn=" + encUrl + "&tr=udp://glotorrents.pw:6969/announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://torrent.gresille.org:80/announce&tr=udp://tracker.openbittorrent.com:80";
-    let mag8 = "magnet:?xt=urn:btih:" + data.torrents[y - 1].hash + "&dn=" + encUrl + "&tr=udp://glotorrents.pw:6969/announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://torrent.gresille.org:80/announce&tr=udp://tracker.openbittorrent.com:80";
-    box = document.createElement("div");
-    box.classList.add("down");
-    box.classList.add("row");
-    box.classList.add("text-center");
-    box.setAttribute("id", "box");
-    box.innerHTML = '<span class="col-12" id="h3">' + data.title + '</span>' +
-        '<div class="col-6 ver"><h4>720p</h4><a href=' + mag7 +
-        '><span onclick="hide()" class="fa fa-magnet"> </span></a><a href="' +
-        data.torrents[y - 2].url +
-        '"><span  onclick="hide()" class="fa fa-download"></span></a><span class="size">Size: ' + data.torrents[y - 2].size + '</span></div>' +
-        '<div class="col-6"><h4>1080p</h4><a href=' + mag8 +
-        '"><span  onclick="hide()" class="fa fa-magnet"> </span></a><a href="' +
-        data.torrents[y - 1].url +
-        '"><span  onclick="hide()" class="fa fa-download"></span></a><span class="size">Size: ' +
-        data.torrents[y - 1].size + '</div><span onclick="hide()"    id="close" class="fa fa-close"></span>';
-    body.appendChild(box);
+  let y = data.torrents.length;
 
+  let mag7 = `magnet:?xt=urn:btih:${
+    data.torrents[y - 2].hash
+  }&dn=${encUrl}&tr=udp://glotorrents.pw:6969/announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://torrent.gresille.org:80/announce&tr=udp://tracker.openbittorrent.com:80`;
+
+  let mag8 = `magnet:?xt=urn:btih:${
+    data.torrents[y - 1].hash
+  }&dn=${encUrl}&tr=udp://glotorrents.pw:6969/announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://torrent.gresille.org:80/announce&tr=udp://tracker.openbittorrent.com:80`;
+
+  box = `
+        <div class="down__con">
+            <div class='down row text-center' id='box'>
+                <span class="col-12" id="h3">${data.title}</span>
+                <div class="col-6 ver">
+                    <h4>720p</h4>
+                    <a onclick="hide()" href='${mag7}'><span class="fa fa-magnet"></span></a>
+                    <a href='${data.torrents[y - 2].url}'  onclick="hide()">
+                        <span  class="fa fa-download"></span>
+                    </a>
+                    <span class="size">Size: ${data.torrents[y - 2].size}
+                    </span>
+                </div>
+                <div class="col-6">
+                    <h4>1080p</h4>
+                    <a  onclick="hide()" href='${mag8}'><span class="fa fa-magnet"> </span></a>
+                    <a href='${data.torrents[y - 1].url}'  onclick="hide()">
+                        <span  class="fa fa-download"></span>
+                    </a>
+                    <span class="size">Size: ${data.torrents[y - 1].size}
+                    </span>
+                </div>
+                <span onclick="hide()" id="close" class="fa fa-close"></span>
+            </div>
+        </div>
+    `;
+  body.insertAdjacentHTML("afterbegin", box);
 }
 
 function hide() {
-    box.style.display = "none";
+  document.querySelector(".down").style.display = "none";
+  hideDownCon();
+}
+
+function hideDownCon() {
+  document.querySelector(".down__con").style.display = "none";
 }
